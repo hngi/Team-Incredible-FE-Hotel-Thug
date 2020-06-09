f65f0e182860b32ca6064b177a42c530e6ce2520
Isaac Sokari <isaacsokari@gmail.com>
2020-06-09 19:45:51 +0100

    feat: started top carousel on homepage

---
+++ scripts/index.js
@@ -0,0 +1,55 @@
+let slideIndex = 0;
+let slides = document.getElementsByClassName('mySlides');
+let dots = document.getElementsByClassName('dot');
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  // console.log(dots.length);
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
+
+// let slideIndex = 0;
+// showSlides();
+
+function startSlides() {
+  var i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+  setTimeout(startSlides, 3000); // Change image every 2 seconds
+}
+
+startSlides();
