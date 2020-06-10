8813407cafeea3fdede0b80a969e69fd7b126e7b
Isaac Sokari <isaacsokari@gmail.com>
2020-06-10 01:28:50 +0100

    feat: completed home page top slideshow

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Start of Top SlideShow/Carousel
 let slideIndex = 0;
 let topSlides = document.getElementsByClassName('topSlides');
 let topDots = document.getElementsByClassName('topDot');
@@ -52,26 +53,5 @@ function startTopSlides() {
 }
 
 startTopSlides();
-// Gallery Slide start
-let slideStart = 0;
-moveSlides();
-
-function moveSlides() {
-    let i;
-    let slides = document.getElementsByClassName('gallery_imgs');
-    let indicator = document.getElementsByClassName('ind_item');
-    for (i=0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-    slideStart++;
-    if (slideStart > slides.length) {
-        slideStart = 1;
-    }
-    for (i = 0; i < indicator.length; i++) {
-        indicator[i].className = indicator[i].className.replace(' focus', '');
-    }
-    slides[slideStart-1].style.display = 'grid';
-    indicator[slideStart-1].className += ' focus';
-    setTimeout(moveSlides, 5000);
-}
+// End of Top SlideShow/Carousel
 
